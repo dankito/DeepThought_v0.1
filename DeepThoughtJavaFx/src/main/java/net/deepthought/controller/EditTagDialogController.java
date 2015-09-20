3eee4f5e88279e6816a6e43236b94bc09c9a8d88
dankito <dankito@user.noreply.github.com>
2015-09-20 10:02:33 +0200

    Derived EditTagDialog from EntityDialogFrame

--- DeepThoughtJavaFx/src/main/java/net/deepthought/controller/EditTagDialogController.java
+++ DeepThoughtJavaFx/src/main/java/net/deepthought/controller/EditTagDialogController.java
@@ -3,56 +3,27 @@ package net.deepthought.controller;
 import net.deepthought.Application;
 import net.deepthought.controller.enums.DialogResult;
 import net.deepthought.controller.enums.FieldWithUnsavedChanges;
-import net.deepthought.controls.Constants;
-import net.deepthought.controls.ContextHelpControl;
-import net.deepthought.controls.FXUtils;
 import net.deepthought.data.model.Tag;
 import net.deepthought.data.model.listener.EntityListener;
 import net.deepthought.data.persistence.db.BaseEntity;
 import net.deepthought.util.Alerts;
-import net.deepthought.util.JavaFxLocalization;
-import net.deepthought.util.Localization;
 
-import java.net.URL;
 import java.util.Collection;
-import java.util.ResourceBundle;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableSet;
-import javafx.collections.SetChangeListener;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
-import javafx.scene.control.ContentDisplay;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleButton;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 /**
  * Created by ganymed on 31/12/14.
  */
-public class EditTagDialogController extends ChildWindowsController implements Initializable {
+public class EditTagDialogController extends EntityDialogFrameController implements Initializable {
 
   protected Tag tag = null;
 
-  protected ObservableSet<FieldWithUnsavedChanges> fieldsWithUnsavedChanges = FXCollections.observableSet();
-
-
-  @FXML
-  protected BorderPane dialogPane;
-
-  @FXML
-  protected Button btnApply;
-
-  @FXML
-  protected ToggleButton tglbtnShowHideContextHelp;
-
-  protected ContextHelpControl contextHelpControl;
 
   @FXML
   protected TextField txtfldName;
@@ -61,34 +32,20 @@ public class EditTagDialogController extends ChildWindowsController implements I
 
 
   @Override
-  public void initialize(URL location, ResourceBundle resources) {
-    btnApply.managedProperty().bind(btnApply.visibleProperty());
-
-    setupFields();
-
-    fieldsWithUnsavedChanges.addListener(new SetChangeListener<FieldWithUnsavedChanges>() {
-      @Override
-      public void onChanged(Change<? extends FieldWithUnsavedChanges> c) {
-        btnApply.setDisable(fieldsWithUnsavedChanges.size() == 0);
-      }
-    });
+  protected String getEntityType() {
+    return "tag";
   }
 
-  protected void setupFields() {
+
+  @Override
+  protected void setupControls() {
+    super.setupControls();
+
     txtfldName.textProperty().addListener((observable, oldValue, newValue) -> fieldsWithUnsavedChanges.add(FieldWithUnsavedChanges.TagName));
     txtfldName.focusedProperty().addListener((observable, oldValue, newValue) -> fieldFocused("name"));
 
     txtfldDescription.textProperty().addListener((observable, oldValue, newValue) -> fieldsWithUnsavedChanges.add(FieldWithUnsavedChanges.TagDescription));
     txtfldDescription.focusedProperty().addListener((observable, oldValue, newValue) -> fieldFocused("description"));
-
-    contextHelpControl = new ContextHelpControl("context.help.tag.");
-    dialogPane.setRight(contextHelpControl);
-
-    FXUtils.ensureNodeOnlyUsesSpaceIfVisible(contextHelpControl);
-    contextHelpControl.visibleProperty().bind(tglbtnShowHideContextHelp.selectedProperty());
-
-    tglbtnShowHideContextHelp.setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
-    tglbtnShowHideContextHelp.setGraphic(new ImageView(Constants.ContextHelpIconPath));
   }
 
   protected void fieldFocused(String fieldName) {
@@ -97,10 +54,8 @@ public class EditTagDialogController extends ChildWindowsController implements I
 
 
   public void setTagAndStage(Stage dialogStage, Tag tagToEdit) {
-    setWindowStage(dialogStage);
     this.tag = tagToEdit;
-
-    updateStageTitle();
+    setWindowStage(dialogStage, tagToEdit);
 
     tagToEditSet(tagToEdit);
     fieldsWithUnsavedChanges.clear();
@@ -115,34 +70,6 @@ public class EditTagDialogController extends ChildWindowsController implements I
     txtfldDescription.setText(tag.getDescription());
   }
 
-  public boolean hasUnsavedChanges() {
-    return fieldsWithUnsavedChanges.size() > 0;
-  }
-
-  protected void updateStageTitle() {
-    if(tag.isPersisted() == false)
-      JavaFxLocalization.bindStageTitle(windowStage, "create.tag");
-    else
-      JavaFxLocalization.bindStageTitle(windowStage, "edit.tag", tag.getTextRepresentation());
-  }
-
-
-  @FXML
-  public void handleButtonApplyAction(ActionEvent actionEvent) {
-    saveEditedFields();
-  }
-
-  @FXML
-  public void handleButtonCancelAction(ActionEvent actionEvent) {
-    closeDialog(DialogResult.Cancel);
-  }
-
-  @FXML
-  public void handleButtonOkAction(ActionEvent actionEvent) {
-    saveEditedFields();
-    closeDialog(DialogResult.Ok);
-  }
-
   @Override
   protected void closeDialog() {
     if(tag != null)
@@ -151,7 +78,7 @@ public class EditTagDialogController extends ChildWindowsController implements I
     super.closeDialog();
   }
 
-  protected void saveEditedFields() {
+  protected void saveEntity() {
     if(fieldsWithUnsavedChanges.contains(FieldWithUnsavedChanges.TagName)) {
       tag.setName(txtfldName.getText());
       fieldsWithUnsavedChanges.remove(FieldWithUnsavedChanges.TagName);
@@ -164,21 +91,6 @@ public class EditTagDialogController extends ChildWindowsController implements I
 
     if(tag.isPersisted() == false)
       Application.getDeepThought().addTag(tag);
-  }
-
-  @Override
-  protected boolean askIfStageShouldBeClosed() {
-    if(hasUnsavedChanges()) {
-      ButtonType result = Alerts.askUserIfEditedEntityShouldBeSaved(windowStage, "tag");
-
-      if(result.equals(ButtonType.CANCEL))
-        return false;
-      else if(result.equals(ButtonType.YES)) {
-        saveEditedFields();
-      }
-    }
-
-    return true;
   }
 
 
