e07dff81a1669c34e95fe5df5696b6afbb0085f6
dankito <dankito@user.noreply.github.com>
2016-04-21 22:58:31 +0200

    SueddeutscheContentExtractor: Added a (commented out) online logo

--- libs/content_extractor/OnlineNewspaperContentExtractor/src/main/java/net/deepthought/data/contentextractor/SueddeutscheContentExtractorBase.java
+++ libs/content_extractor/OnlineNewspaperContentExtractor/src/main/java/net/deepthought/data/contentextractor/SueddeutscheContentExtractorBase.java
@@ -1,11 +1,6 @@
 package net.deepthought.data.contentextractor;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public abstract class SueddeutscheContentExtractorBase extends OnlineNewspaperContentExtractorBase {
-
-  private final static Logger log = LoggerFactory.getLogger(SueddeutscheContentExtractorBase.class);
 
   protected static final String LogoFileName = "sz_logo.png";
 
@@ -16,6 +11,7 @@ public abstract class SueddeutscheContentExtractorBase extends OnlineNewspaperCo
 
   @Override
   public String getIconUrl() {
+//    return "http://polpix.sueddeutsche.com/staticassets/img/touch-icon-ipad-retina.png";
     return tryToLoadIconFile(LogoFileName);
   }
 
