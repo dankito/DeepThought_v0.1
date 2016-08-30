f6ca32e475a8965e275efab12dd3480087e21134
dankito <dankito@user.noreply.github.com>
2016-08-30 14:40:30 +0200

    Starting synchronization now asynchronously

--- DeepThoughtLib/src/main/java/net/dankito/deepthought/data/sync/SyncManagerBase.java
+++ DeepThoughtLib/src/main/java/net/dankito/deepthought/data/sync/SyncManagerBase.java
@@ -1,5 +1,6 @@
 package net.dankito.deepthought.data.sync;
 
+import net.dankito.deepthought.Application;
 import net.dankito.deepthought.communication.IDeepThoughtConnector;
 import net.dankito.deepthought.communication.connected_device.IConnectedDevicesListener;
 import net.dankito.deepthought.communication.model.ConnectedDevice;
@@ -25,14 +26,24 @@ public abstract class SyncManagerBase implements IDeepThoughtSyncManager {
   protected abstract void stopSynchronizationWithDevice(ConnectedDevice device);
 
 
+  protected void startSynchronizationWithDeviceAsync(final ConnectedDevice device) {
+    Application.getThreadPool().runTaskAsync(new Runnable() {
+      @Override
+      public void run() {
+        try {
+          startSynchronizationWithDevice(device);
+        } catch(Exception e) {
+          log.error("Could not start Synchronization with Device " + device); // TODO: inform User (e.g. over a Notification)
+        }
+      }
+    });
+  }
+
+
   protected IConnectedDevicesListener connectedDevicesListener = new IConnectedDevicesListener() {
     @Override
     public void registeredDeviceConnected(ConnectedDevice device) {
-      try {
-        startSynchronizationWithDevice(device);
-      } catch(Exception ex) {
-        log.error("Could not start Synchronization with Device " + device); // TODO: inform User (e.g. over a Notification)
-      }
+      startSynchronizationWithDeviceAsync(device);
     }
 
     @Override
