6eecc0a82072a13ba1396446a66d04f1af2994ea
dankito <dankito@user.noreply.github.com>
2016-09-02 15:43:14 +0200

    On shutdown() sending that we are disconnecting to all connected devices

--- DeepThoughtLib/src/main/java/net/dankito/deepthought/communication/DeepThoughtConnector.java
+++ DeepThoughtLib/src/main/java/net/dankito/deepthought/communication/DeepThoughtConnector.java
@@ -108,6 +108,8 @@ public class DeepThoughtConnector implements IDeepThoughtConnector {
 
   @Override
   public void shutDown() {
+    disconnectFromAllConnectedDevices();
+
     stopMessagesReceiver();
 
     stopDevicesFinder();
