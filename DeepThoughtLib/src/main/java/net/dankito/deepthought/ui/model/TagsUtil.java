ec726621420d317c561ef8e76edfc5aecd503ccb
dankito <dankito@user.noreply.github.com>
2016-09-12 18:09:57 +0200

    Extracted TagsUtil from EditEntryActivity

---
+++ DeepThoughtLib/src/main/java/net/dankito/deepthought/ui/model/TagsUtil.java
@@ -0,0 +1,46 @@
+package net.dankito.deepthought.ui.model;
+
+import net.dankito.deepthought.data.model.Tag;
+import net.dankito.deepthought.util.localization.Localization;
+
+import java.util.Collection;
+
+/**
+ * Created by ganymed on 12/09/16.
+ */
+public class TagsUtil {
+
+  public static final int NO_MAX_PREVIEW_LENGTH = -1;
+
+
+  public String createTagsPreview(Collection<Tag> tags, boolean showNoTagsSetMessage) {
+    return createTagsPreview(tags, showNoTagsSetMessage, NO_MAX_PREVIEW_LENGTH);
+  }
+
+  public String createTagsPreview(Collection<Tag> tags, boolean showNoTagsSetMessage, int maxPreviewLength) {
+    String tagsPreview = "";
+
+    if(tags.size() == 0) {
+      if(showNoTagsSetMessage) {
+        tagsPreview = Localization.getLocalizedString("no.tags.set");
+      }
+    }
+    else {
+      for (Tag tag : tags) {
+        tagsPreview += tag.getName() + ", ";
+      }
+
+      if (tagsPreview.length() > 1) { // remove last ", "
+        tagsPreview = tagsPreview.substring(0, tagsPreview.length() - 2);
+      }
+
+      if (maxPreviewLength > 0 && tagsPreview.length() >= maxPreviewLength) {
+        tagsPreview = tagsPreview.substring(0, maxPreviewLength) + " ...";
+      }
+
+      tagsPreview = tagsPreview.replace("\r", "").replace("\n", ""); // if a Tag contains a new line symbol, replace that one for preview
+    }
+
+    return tagsPreview;
+  }
+}
