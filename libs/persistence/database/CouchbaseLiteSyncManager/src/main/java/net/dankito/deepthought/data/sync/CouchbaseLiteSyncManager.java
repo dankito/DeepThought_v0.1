0c9095b2ca972da9eebccaaf10bddc6654eb5a8c
dankito <dankito@user.noreply.github.com>
2016-09-14 15:57:58 +0200

    Added Synchronization Filter and additional log output

--- libs/persistence/database/CouchbaseLiteSyncManager/src/main/java/net/dankito/deepthought/data/sync/CouchbaseLiteSyncManager.java
+++ libs/persistence/database/CouchbaseLiteSyncManager/src/main/java/net/dankito/deepthought/data/sync/CouchbaseLiteSyncManager.java
@@ -3,6 +3,8 @@ package net.dankito.deepthought.data.sync;
 import com.couchbase.lite.Database;
 import com.couchbase.lite.DocumentChange;
 import com.couchbase.lite.Manager;
+import com.couchbase.lite.ReplicationFilter;
+import com.couchbase.lite.SavedRevision;
 import com.couchbase.lite.listener.Credentials;
 import com.couchbase.lite.listener.LiteListener;
 import com.couchbase.lite.replicator.Replication;
@@ -10,6 +12,11 @@ import com.couchbase.lite.replicator.Replication;
 import net.dankito.deepthought.communication.Constants;
 import net.dankito.deepthought.communication.connected_device.IConnectedDevicesListenerManager;
 import net.dankito.deepthought.communication.model.ConnectedDevice;
+import net.dankito.deepthought.data.model.DeepThoughtApplication;
+import net.dankito.deepthought.data.model.Device;
+import net.dankito.deepthought.data.model.Group;
+import net.dankito.deepthought.data.model.User;
+import net.dankito.deepthought.data.model.enums.ApplicationLanguage;
 import net.dankito.deepthought.data.persistence.CouchbaseLiteEntityManagerBase;
 import net.dankito.deepthought.data.persistence.db.BaseEntity;
 import net.dankito.deepthought.util.IThreadPool;
@@ -20,11 +27,15 @@ import org.slf4j.LoggerFactory;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class CouchbaseLiteSyncManager extends SyncManagerBase {
+
+  protected static final String FILTER_NAME = "ENTITIES_FILTER";
+
 
   private static final Logger log = LoggerFactory.getLogger(CouchbaseLiteSyncManager.class);
 
@@ -63,6 +74,26 @@ public class CouchbaseLiteSyncManager extends SyncManagerBase {
     this.alsoUsePullReplication = alsoUsePullReplication;
 
     this.dataMerger = new SynchronizedDataMerger(entityManager, database);
+
+    setReplicationFilter(database);
+  }
+
+  private void setReplicationFilter(Database database) {
+    final List<String> entitiesToFilter = new ArrayList<>();
+    entitiesToFilter.add(DeepThoughtApplication.class.getName());
+    entitiesToFilter.add(ApplicationLanguage.class.getName());
+    entitiesToFilter.add(User.class.getName());
+    entitiesToFilter.add(Group.class.getName());
+    entitiesToFilter.add(Device.class.getName());
+
+    database.setFilter(FILTER_NAME, new ReplicationFilter() {
+      @Override
+      public boolean filter(SavedRevision revision, Map<String, Object> params) {
+        // TODO: also check if owner matches loggedOnUser (or if loggedOnUser is allowed to sync a Document from that owner)
+        String entityType = (String)revision.getProperty(Dao.TYPE_COLUMN_NAME);
+        return ! entitiesToFilter.contains(entityType);
+      }
+    });
   }
 
 
@@ -84,6 +115,8 @@ public class CouchbaseLiteSyncManager extends SyncManagerBase {
 
 
   protected void startCBLListener(int listenPort, Manager manager, Credentials allowedCredentials) throws Exception {
+    log.info("Starting Couchbase Lite Listener");
+
     couchbaseLiteListener = new LiteListener(manager, listenPort, allowedCredentials);
     synchronizationPort = couchbaseLiteListener.getListenPort();
 
@@ -92,6 +125,8 @@ public class CouchbaseLiteSyncManager extends SyncManagerBase {
   }
 
   protected void stopCBLListener() {
+    log.info("Stopping Couchbase Lite Listener");
+
     if(listenerThread != null) {
       try { listenerThread.join(500); } catch(Exception ignored) { }
 
@@ -127,6 +162,8 @@ public class CouchbaseLiteSyncManager extends SyncManagerBase {
   }
 
   protected void startReplication(ConnectedDevice device) throws Exception {
+    log.info("Starting Replication with Device " + device);
+
     URL syncUrl;
     try {
       int remoteDeviceSyncPort = device.getSynchronizationPort();
@@ -136,6 +173,7 @@ public class CouchbaseLiteSyncManager extends SyncManagerBase {
     }
 
     Replication pushReplication = database.createPushReplication(syncUrl);
+    pushReplication.setFilter(FILTER_NAME);
     pushReplication.addChangeListener(replicationChangeListener);
     pushReplication.setContinuous(true);
 
@@ -145,6 +183,7 @@ public class CouchbaseLiteSyncManager extends SyncManagerBase {
 
     if (alsoUsePullReplication) {
       Replication pullReplication = database.createPullReplication(syncUrl);
+      pullReplication.setFilter(FILTER_NAME);
       pullReplication.addChangeListener(replicationChangeListener);
       pullReplication.setContinuous(true);
 
@@ -159,6 +198,8 @@ public class CouchbaseLiteSyncManager extends SyncManagerBase {
   @Override
   protected void stopSynchronizationWithDevice(ConnectedDevice device) {
     synchronized(this) {
+      log.info("Stopping Replication with Device " + device);
+
       Replication pullReplication = pullReplications.remove(getDeviceKey(device));
       if(pullReplication != null) {
         pullReplication.stop();
