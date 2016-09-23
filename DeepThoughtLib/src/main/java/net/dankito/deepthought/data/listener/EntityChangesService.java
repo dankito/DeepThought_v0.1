a7fc8f88ec5319a502104897dbd11c6ec257f8ca
dankito <dankito@user.noreply.github.com>
2016-09-23 22:30:29 +0200

    Removed IExternalCallableEntityChangesService

--- DeepThoughtLib/src/main/java/net/dankito/deepthought/data/listener/EntityChangesService.java
+++ DeepThoughtLib/src/main/java/net/dankito/deepthought/data/listener/EntityChangesService.java
@@ -14,7 +14,7 @@ import java.util.concurrent.CopyOnWriteArraySet;
 /**
  * Created by ganymed on 20/09/16.
  */
-public class EntityChangesService implements IExternalCallableEntityChangesService {
+public class EntityChangesService implements IEntityChangesService {
 
   protected Set<AllEntitiesListener> allEntitiesListeners = new CopyOnWriteArraySet<>();
 
@@ -34,32 +34,6 @@ public class EntityChangesService implements IExternalCallableEntityChangesServi
   @Override
   public boolean removeAllEntitiesListener(AllEntitiesListener listener) {
     return allEntitiesListeners.remove(listener);
-  }
-
-
-  @Override
-  public void informEntityCreatedListeners(BaseEntity entity) {
-    callEntityCreatedListeners(entity);
-  }
-
-  @Override
-  public void informEntityUpdatedListeners(BaseEntity entity, String propertyName, Object previousValue, Object newValue) {
-    callEntityUpdatedListeners(entity, propertyName, previousValue, newValue);
-  }
-
-  @Override
-  public void informEntityDeletedListeners(BaseEntity entity) {
-    callEntityDeletedListeners(entity);
-  }
-
-  @Override
-  public void informEntityAddedToCollectionListeners(BaseEntity collectionHolder, Collection<? extends BaseEntity> collection, BaseEntity addedEntity) {
-    callEntityAddedToCollectionListeners(collectionHolder, collection, addedEntity);
-  }
-
-  @Override
-  public void informEntityRemovedFromCollectionListeners(BaseEntity collectionHolder, Collection<? extends BaseEntity> collection, BaseEntity addedEntity) {
-    callEntityRemovedFromCollectionListeners(collectionHolder, collection, addedEntity);
   }
 
 
