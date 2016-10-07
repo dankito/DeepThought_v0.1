fbc7fa93066b8cd8b124ac62db5b456426358016
dankito <dankito@user.noreply.github.com>
2016-10-07 22:41:50 +0200

    Fixed Dates and Content Lengths

--- libs/content_extractor/OnlineNewspaperContentExtractor/src/test/java/net/dankito/deepthought/data/contentextractor/SueddeutscheMagazinContentExtractorTest.java
+++ libs/content_extractor/OnlineNewspaperContentExtractor/src/test/java/net/dankito/deepthought/data/contentextractor/SueddeutscheMagazinContentExtractorTest.java
@@ -20,28 +20,28 @@ public class SueddeutscheMagazinContentExtractorTest extends GermanOnlineNewspap
   @Test
   public void importNummerEinsDerWocheDruckluftArticle() {
     EntryCreationResult creationResult = testImportArticle("http://sz-magazin.sueddeutsche.de/texte/anzeigen/43365");
-      testImportedArticleValues(creationResult, 3179, "22.07.2015", "Der Laden, in dem sich die Leute am liebsten aufhalten, ist laut einer aktuellen Umfrage: die Buchhandlung.", "Druckluft",
+      testImportedArticleValues(creationResult, 3358, "22.07.15", "Der Laden, in dem sich die Leute am liebsten aufhalten, ist laut einer aktuellen Umfrage: die Buchhandlung.", "Druckluft",
           "Der Laden, in dem sich die Leute am liebsten aufhalten, ist laut einer aktuellen Umfrage: die Buchhandlung. Und die Gelegenheit, bei der die Leute am liebsten lügen, ist erfahrungsgemäß: die Umfrage.");
   }
 
   @Test
   public void importDieSchoenstenStrandspaziergaengeDerWeltArticle() {
     EntryCreationResult creationResult = testImportArticle("http://sz-magazin.sueddeutsche.de/texte/anzeigen/43306/Deine-Spuren-im-Sand");
-    testImportedArticleValues(creationResult, 6452, "28/2015", "Die schönsten Strandspaziergänge der Welt", "Wellengang",
+    testImportedArticleValues(creationResult, 6619, "28/2015", "Die schönsten Strandspaziergänge der Welt", "Wellengang",
         "Die schönsten Strandspaziergänge der Welt: Denn nirgends kommt man besser zur Ruhe als am Ufer des Meeres.");
   }
 
   @Test
   public void importDasZerquetschenVonEiernArticle() {
     EntryCreationResult creationResult = testImportArticle("http://sz-magazin.sueddeutsche.de/texte/anzeigen/42288/Das-Zerquetschen-von-Eiern");
-    testImportedArticleValues(creationResult, 9044, "41/2014", "Straßenprostitution in Spanien - Fotostrecke von Txema Salvans", "Das Zerquetschen von Eiern",
+    testImportedArticleValues(creationResult, 9211, "41/2014", "Straßenprostitution in Spanien - Fotostrecke von Txema Salvans", "Das Zerquetschen von Eiern",
         "Als unser Autor diese Fotos von spanischen Prostituierten sah, wurde ihm klar, wo das Problem mit Männern wie ihm liegt.");
   }
 
   @Test
   public void importArticleWithTwoArtikelElements() {
     EntryCreationResult creationResult = testImportArticle("http://sz-magazin.sueddeutsche.de/texte/anzeigen/43533/Ein-Pausenknopf-fuer-das-Leben");
-    testImportedArticleValues(creationResult, 7815, "04.09.2015", "Der britische Komponist Max Richter steht vor einer besonderen Uraufführung", "»Ein Pausenknopf für das Leben«",
+    testImportedArticleValues(creationResult, 7815, "04.09.15", "Der britische Komponist Max Richter steht vor einer besonderen Uraufführung", "»Ein Pausenknopf für das Leben«",
         "Der britische Komponist Max Richter steht vor einer besonderen Uraufführung: Sein neuestes Werk »Sleep« dauert acht Stunden, im Zuschauerraum stehen 500 Feldbetten. Denn es ist zum Einschlafen.");
   }
 
